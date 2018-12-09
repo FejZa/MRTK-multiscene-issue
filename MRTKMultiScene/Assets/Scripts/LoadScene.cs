bef16ac171963b1f09a4bc81ce9527d96c35e40b
Dino Fejzagic <dino.f@live.de>
2018-12-09 13:54:13 +0100

    Push example project for issue

---
+++ MRTKMultiScene/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("B");
+    }
+}
